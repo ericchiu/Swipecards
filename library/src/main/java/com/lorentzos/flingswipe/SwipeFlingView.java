7a9a09301de37e7fafe9bfc62d78b6236f4f85a4
eric <eric@netcosports.com>
2014-11-29 18:08:14 +0100

    swipeflingview added

---
+++ library/src/main/java/com/lorentzos/flingswipe/SwipeFlingView.java
@@ -0,0 +1,292 @@
+package com.lorentzos.flingswipe;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+/**
+ * Created by dionysis_lorentzos on 5/8/14
+ * for package com.lorentzos.swipecards
+ * and project Swipe cards.
+ * Use with caution dinosaurs might appear!
+ */
+
+public class SwipeFlingView extends FrameLayout {
+    private float ROTATION_DEGREES = 15.f;
+
+    private onFlingListener mFlingListener;
+    private boolean mInLayout = false;
+    private View mActiveCard = null;
+    private OnItemClickListener mOnItemClickListener;
+    private FlingCardListener flingCardListener;
+
+    private int heightMeasureSpec;
+    private int widthMeasureSpec;
+
+    private int mLayoutRessource;
+
+    public SwipeFlingView(Context context) {
+        this(context, null);
+    }
+
+    public SwipeFlingView(Context context, AttributeSet attrs) {
+        this(context, attrs, R.attr.SwipeFlingStyle);
+    }
+
+    public SwipeFlingView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.SwipeFlingAdapterView, defStyle, 0);
+        ROTATION_DEGREES = a.getFloat(R.styleable.SwipeFlingAdapterView_rotation_degrees, ROTATION_DEGREES);
+        a.recycle();
+    }
+
+
+    /**
+     * A shortcut method to set both the listeners and the adapter.
+     *
+     * @param context The activity context which extends onFlingListener, OnItemClickListener or both
+     */
+    public void init(final Context context) {
+        if (context instanceof onFlingListener) {
+            mFlingListener = (onFlingListener) context;
+        } else {
+            throw new RuntimeException("Activity does not implement SwipeFlingAdapterView.onFlingListener");
+        }
+        if (context instanceof OnItemClickListener) {
+            mOnItemClickListener = (OnItemClickListener) context;
+        }
+    }
+
+    public View getSelectedView() {
+        return mActiveCard;
+    }
+
+    @Override
+    public void requestLayout() {
+        if (!mInLayout) {
+            super.requestLayout();
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        mInLayout = true;
+
+        if (getChildAt(0) == null) {
+            removeAllViewsInLayout();
+            setTopView(layoutChildren());
+        }
+
+        mInLayout = false;
+    }
+
+
+    private View layoutChildren() {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View newUnderChild = inflater.inflate(mLayoutRessource, this, false);
+        if (newUnderChild.getVisibility() != GONE) {
+            makeAndAddView(newUnderChild);
+        }
+        return newUnderChild;
+
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void makeAndAddView(View child) {
+
+        FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) child.getLayoutParams();
+        addViewInLayout(child, 0, lp, true);
+
+        final boolean needToMeasure = child.isLayoutRequested();
+        if (needToMeasure) {
+            int childWidthSpec = getChildMeasureSpec(getWidthMeasureSpec(),
+                    getPaddingLeft() + getPaddingRight() + lp.leftMargin + lp.rightMargin,
+                    lp.width);
+            int childHeightSpec = getChildMeasureSpec(getHeightMeasureSpec(),
+                    getPaddingTop() + getPaddingBottom() + lp.topMargin + lp.bottomMargin,
+                    lp.height);
+            child.measure(childWidthSpec, childHeightSpec);
+        } else {
+            cleanupLayoutState(child);
+        }
+
+
+        int w = child.getMeasuredWidth();
+        int h = child.getMeasuredHeight();
+
+        int gravity = lp.gravity;
+        if (gravity == -1) {
+            gravity = Gravity.TOP | Gravity.START;
+        }
+
+
+        int layoutDirection = getLayoutDirection();
+        final int absoluteGravity = Gravity.getAbsoluteGravity(gravity, layoutDirection);
+        final int verticalGravity = gravity & Gravity.VERTICAL_GRAVITY_MASK;
+
+        int childLeft;
+        int childTop;
+        switch (absoluteGravity & Gravity.HORIZONTAL_GRAVITY_MASK) {
+            case Gravity.CENTER_HORIZONTAL:
+                childLeft = (getWidth() + getPaddingLeft() - getPaddingRight() - w) / 2 +
+                        lp.leftMargin - lp.rightMargin;
+                break;
+            case Gravity.END:
+                childLeft = getWidth() + getPaddingRight() - w - lp.rightMargin;
+                break;
+            case Gravity.START:
+            default:
+                childLeft = getPaddingLeft() + lp.leftMargin;
+                break;
+        }
+        switch (verticalGravity) {
+            case Gravity.CENTER_VERTICAL:
+                childTop = (getHeight() + getPaddingTop() - getPaddingBottom() - h) / 2 +
+                        lp.topMargin - lp.bottomMargin;
+                break;
+            case Gravity.BOTTOM:
+                childTop = getHeight() - getPaddingBottom() - h - lp.bottomMargin;
+                break;
+            case Gravity.TOP:
+            default:
+                childTop = getPaddingTop() + lp.topMargin;
+                break;
+        }
+
+        child.layout(childLeft, childTop, childLeft + w, childTop + h);
+    }
+
+
+    /**
+     * Set the top view and add the fling listener
+     */
+    private void setTopView(View view) {
+        if (getChildCount() > 0) {
+
+            mActiveCard = view;
+            if (mActiveCard != null) {
+
+                flingCardListener = new FlingCardListener(mActiveCard, null,
+                        ROTATION_DEGREES, new FlingCardListener.FlingListener() {
+
+                    @Override
+                    public void onCardExited() {
+                        mFlingListener.removeFirstObjectInAdapter();
+                    }
+
+                    @Override
+                    public void leftExit(Object dataObject) {
+                        mFlingListener.onLeftCardExit(dataObject);
+                    }
+
+                    @Override
+                    public void rightExit(Object dataObject) {
+                        mFlingListener.onRightCardExit(dataObject);
+                    }
+
+                    @Override
+                    public void onClick(Object dataObject) {
+                        if (mOnItemClickListener != null)
+                            mOnItemClickListener.onItemClicked(0, dataObject);
+
+                    }
+
+                    @Override
+                    public void onUpdate(int left) {
+                        mFlingListener.onUpdate(left);
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        mFlingListener.onCancel();
+                    }
+                });
+                mFlingListener.onNewActiveCard(mActiveCard);
+                mActiveCard.setOnTouchListener(flingCardListener);
+            }
+        }
+    }
+
+    public FlingCardListener getTopCardListener() throws NullPointerException {
+        if (flingCardListener == null) {
+            throw new NullPointerException();
+        }
+        return flingCardListener;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        this.widthMeasureSpec = widthMeasureSpec;
+        this.heightMeasureSpec = heightMeasureSpec;
+    }
+
+    public int getWidthMeasureSpec() {
+        return widthMeasureSpec;
+    }
+
+    public int getHeightMeasureSpec() {
+        return heightMeasureSpec;
+    }
+
+    public void setFlingListener(onFlingListener onFlingListener) {
+        this.mFlingListener = onFlingListener;
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.mOnItemClickListener = onItemClickListener;
+    }
+
+
+    @Override
+    public LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new FrameLayout.LayoutParams(getContext(), attrs);
+    }
+
+
+    public interface OnItemClickListener {
+        public void onItemClicked(int itemPosition, Object dataObject);
+    }
+
+    public interface onFlingListener {
+        public void removeFirstObjectInAdapter();
+
+        public void onLeftCardExit(Object dataObject);
+
+        public void onRightCardExit(Object dataObject);
+
+        public void onUpdate(int left);
+
+        public void onCancel();
+
+        public void onNewActiveCard(View activeCard);
+
+    }
+
+    public void setEnable(boolean enable) {
+        if (flingCardListener != null) {
+            flingCardListener.setEnable(enable);
+        }
+    }
+
+    public void setLayoutRessource(int layoutressource) {
+        this.mLayoutRessource = layoutressource;
+    }
+
+    public void reset(){
+        if(mActiveCard != null){
+            mActiveCard.setY(0);
+            mActiveCard.setX(0);
+        }
+    }
+}
